985c532df55d3bc01667273dc026926b0d9a5ad4
ananda bayu <anandabayu090@gmail.com>
2022-09-10 20:03:33 +0700

    fix-2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
-// import { Outlet } from 'react-router-dom';
 import ListMahasiswa from './pages/ListMahasiswa';
 
 function App() {
   return (
     <div >
-      {/* <Outlet /> */}
       <ListMahasiswa />
     </div>
   );
